d91fb0cd47a59bfd2246c8e531d9dd82597b9aa6
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-16 10:37:48 +0700

    fix:notification repository

--- src/main/java/com/amu/notification/repositories/NotificationRepository.java
+++ src/main/java/com/amu/notification/repositories/NotificationRepository.java
@@ -1,7 +1,7 @@
 package com.amu.notification.repositories;
 
-import com.amu.notification.kafka.payment.PaymentConfirmation;
+import com.amu.notification.entities.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface NotificationRepository extends MongoRepository<PaymentConfirmation, String> {
+public interface NotificationRepository extends MongoRepository<Notification, String> {
 }
