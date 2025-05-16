00f15c90fa77e0d679b873a761cc9413f768d269
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-16 10:08:49 +0700

    feat: add Product for list product field to OrderConfirmation record

---
+++ src/main/java/com/amu/notification/dto/Product.java
@@ -0,0 +1,12 @@
+package com.amu.notification.dto;
+
+import java.math.BigDecimal;
+
+public record Product(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
