d3ebd530925ba2cc0d67af0ff7ab194fc4f2dc85
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-05-17 17:15:08 +0700

    feat: config notification service yml

--- src/main/java/com/amu/notification/kafka/NotificationConsumer.java
+++ src/main/java/com/amu/notification/kafka/NotificationConsumer.java
@@ -10,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
-
 import static com.amu.notification.entities.NotificationType.ORDER_CONFIRMATION;
 import static com.amu.notification.entities.NotificationType.PAYMENT_CONFIRMATION;
 
